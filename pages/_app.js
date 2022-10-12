e80af882bf8bbf23f65e71b6cf3cfbf2339b77c2
Raul Rodrigues <raulrodriguesdemoraes@gmail.com>
2022-10-12 17:30:35 -0500

    [ADDED] .

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
+import { ThemeProvider } from 'styled-components'
 import '../styles/globals.css'
+import { lightTheme, darkTheme } from './theme';
+import { GlobalStyles } from './global';
+
+import Navbar from './components/Navbar';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <Navbar />
+      <Component {...pageProps} />
+    </ThemeProvider>)
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ import Image from 'next/image';
 
 const Home = () => {
     return (
+
         <div className={styles.container}>
             <Head>
                 <title>RR - Raul Rodrigues</title>
@@ -15,46 +16,32 @@ const Home = () => {
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css"></link>
             </Head>
             <main className={styles.main}>
-                <Image
-                    src="/logo.svg"
-                    width={100}
-                    height={100}
-                    alt='Disponível na PlayStore'
-                />
                 <h1 className={styles.title}>
                     Olá! Eu sou <b>Raul Rodrigues</b>.
                 </h1>
-
                 <p className={styles.description}>
-                    Apenas um
-                    <code className={styles.code}>Designer Gráfico & desenvolvedor full-stack web/mobile.</code>
+                    <code className={styles.code}>Designer Gráfico & desenvolvedor full-stack web/mobile</code>
                 </p>
-
                 <div>
                     <Image
                         src="https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E"
                         width={150}
                         height={40}
-                        alt='JavaScript'
-                    />
+                        alt='JavaScript' />
                     <Image
                         src="https://camo.githubusercontent.com/a1eae878fdd3d1c1b687992ca74e5cac85f4b68e60a6efaa7bc8dc9883b71229/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f4e6f64652e6a732d3333393933333f7374796c653d666f722d7468652d6261646765266c6f676f3d6e6f6465646f746a73266c6f676f436f6c6f723d7768697465"
                         width={150}
                         height={40}
-                        alt='Nodejs'
-                    />
+                        alt='Nodejs' />
                     <Image
                         src="https://camo.githubusercontent.com/268ac512e333b69600eb9773a8f80b7a251f4d6149642a50a551d4798183d621/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f52656163742d3230323332413f7374796c653d666f722d7468652d6261646765266c6f676f3d7265616374266c6f676f436f6c6f723d363144414642"
                         width={150}
                         height={40}
-                        alt='React'
-                    />
+                        alt='React' />
                 </div>
-
                 <p className={styles.description}>
                     <code className={styles.code}>PROJETOS:</code>
                 </p>
-
                 <div className={styles.grid}>
                     <a href="https://www.dlwalt.com" className={styles.card}>
                         <h2 className={styles.badge}><i class="devicon-android-plain"></i></h2>
@@ -70,12 +57,13 @@ const Home = () => {
                             src="/pt-br_get.svg"
                             width={100}
                             height={100}
-                            alt='Disponível na PlayStore'
-                        />
+                            alt='Disponível na PlayStore' />
                     </a>
                 </div>
+                <p className={styles.description}>
+                    <code className={styles.code}>CONTATO:</code>
+                </p>
             </main>
-
             <footer className={styles.footer}>
                 &copy;
                 {new Date().getFullYear()} {' '}
